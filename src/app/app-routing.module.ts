95a7d85b245189fd59688a1d3c4f22540da1f295
diniesyahirah <dinie.syahirah@gofreightz.com>
2023-01-03 11:49:33 +0800

    Add new Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MainPageComponent } from './dashboard/main-page/main-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'main', component: MainPageComponent}
+  { path: 'dashboard', component: DashboardComponent }
+  // { path: 'main', component: MainPageComponent}
 ];
 
 @NgModule({
