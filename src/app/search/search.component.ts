8058ac65cf3ad45d99b7ba1770285789fd5e0235
diniesyahirah <dinie.syahirah@gofreightz.com>
2023-01-04 16:51:45 +0800

    Add Search Page

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import SearchData from './search.json';
 
+interface content {
+  img : string,
+  title : string
+}
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  listData = SearchData;
+  browseAll : any = [];
   constructor() { }
 
   ngOnInit(): void {
+
+    this.browseAll = this.listData.Browse;
+
+    this.browseAll.forEach( (element : any) => {
+      var color = Math.floor(0x1000000 * Math.random()).toString(16);
+       element.color= '#' + ('000000' + color).slice(-6);
+
+      console.log(element)
+    });
   }
 
 }
